6af4a3c3a572daaa2225ac63b85bded014161912
Drashan U <darshan.umapathi@outlook.com>
2020-11-02 02:48:46 +0000

    updates for 2-11 mesh

--- Project_Tcd/Texture.h
+++ Project_Tcd/Texture.h
@@ -71,4 +71,45 @@ public:
 	}
 
 	inline GLint getID() const { return this->id; }
+
+	inline GLint getTxtureunit() const{ return this->textureunit; }
+
+	void LoadfromFile(const char* FileName)
+	{
+		if (this->id)
+		{
+			glDeleteTextures(1, &this->id);
+		}
+		this->textureunit = textureunit;
+		this->type = type;
+		unsigned char* image = SOIL_load_image(FileName, &this->width, &this->height, NULL, SOIL_LOAD_RGBA);
+		// We create a texture id to load the images 
+		//unsigned int Texture0;
+		//generate textures and bind it
+		glGenTextures(1, &this->id);
+		glBindTexture(this->type, this->id);
+
+		//glTexParamteri() -- maping images to x and y coordinates in Textures (S and T)
+		glTexParameteri(this->type, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(this->type, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(this->type, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(this->type, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		if (image)
+		{
+			std::cout << "Image Loaded successfully" << std::endl;
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+			//glGenerateMipmap - makes smaller and bigger version of image we have given (Several sizes and 
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+		}
+		else {
+			std::cout << "Error While Loading Image : Texture loading failed" << FileName << std::endl;
+		}
+
+		glActiveTexture(0);
+		glBindTexture(this->type, 0);
+		SOIL_free_image_data(image);
+	}
+
 };
